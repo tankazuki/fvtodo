0aee5738508e371f5d1c1b9790ed1ef0e0389630
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-02-13 00:22:14 +0900

    axioでbackendから値を取得

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,5 +2,32 @@
 <template>
   <div>
     <p>HomePage</p>
+    <p>Random number from backend: {{ randomNumber }}</p>
+    <button @click="getRandom">New Random Number</button> 
   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      randomNumber: 0
+    }
+  },
+  methods: {
+    getRandom () {
+      const path = 'http://localhost:5000/api/random'
+      axios.get(path)
+      .then(response => {
+        this.randomNumber = response.data.randomNumber
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  },
+  created () {
+    this.getRandom()
+  }
+}
+</script>
